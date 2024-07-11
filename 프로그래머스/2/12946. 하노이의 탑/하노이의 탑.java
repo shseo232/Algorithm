1532eb9e2a78d3242eb327c79e2f508b45603d5e
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-07-11 21:41:27 +0900

    [level 2] Title: 하노이의 탑, Time: 2.72 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    static List<int[]> list = new ArrayList();
+    public static void move(int n,int from, int to){
+        if(n ==1){
+            list.add(new int[] {from,to});
+            return;
+        }
+        
+        int empty = 6 - from - to;
+        move(n-1,from,empty);
+        move(1,from,to);
+        move(n-1,empty,to);
+    
+    }
+    public List<int[]> solution(int n) {
+        move(n,1,3);
+        return list;
+    }
+}
