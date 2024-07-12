e998bce6ae7c08f321e746305dc36c8ba3196e3c
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-07-12 20:53:56 +0900

    [Silver V] Title: 소트인사이드, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<Integer>();
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            list.add(Integer.parseInt(s.charAt(i) + ""));
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+        }
+        System.out.println(sb);
+    }
+}
