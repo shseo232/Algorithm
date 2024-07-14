494ae54647fb66cd168181b47be8469e65c35992
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-07-14 14:25:55 +0900

    [level 1] Title: K번째수, Time: 0.27 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++){
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int target = commands[i][2];
+            List<Integer> list = new ArrayList();
+            for(int j=start-1; j<=end-1; j++){
+            list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i]=list.get(target-1);
+        }
+        return answer;
+    }
+}
