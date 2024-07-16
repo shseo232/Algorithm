2b34dba8dcc509b1bbe5b4b2d130540645764c0c
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-07-16 19:49:17 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 108 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -6,15 +6,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        if(N==0){
-            System.out.println("1");
-        }else {
-            Long a= 1L;
-            for (int i = 1; i <= N; i++) {
-                a *= i;
-            }
-            System.out.println(a);
-        }
-
+        System.out.println(Fac(N));
     }
+    public static long Fac (int N) {
+        if(N == 0){
+            return 1;
+        }
+          return N * Fac(N-1);
+        }
 }
