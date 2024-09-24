eadcc043fb48b9443840a99038ee95d72d02bf15
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-09-24 17:03:59 +0900

    [level 1] Title: 모의고사, Time: 2.96 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,30 @@
 import java.util.*;
 class Solution {
+    static int[] count = {0,0,0};
     public List<Integer> solution(int[] answers) {
         int[] answer = {};
-        List<Integer> list = new ArrayList();
-        int[] check = new int[3];
-        int[] a = {1,2,3,4,5};
-        int[] b = {2,1,2,3,2,4,2,5};
-        int[] c = {3,3,1,1,2,2,4,4,5,5};    
-        check[0] = cal(a,answers);
-        check[1] = cal(b,answers);
-        check[2] = cal(c,answers);
-        int[] arr = {check[0],check[1],check[2]};
-        Arrays.sort(arr);
-        int max = arr[(arr.length)-1];
-        for(int i=0; i<check.length; i++){
-            if(max == check[i]){
+        int[] player1 = {1,2,3,4,5};
+        int[] player2 = {2,1,2,3,2,4,2,5};
+        int[] player3 = {3,3,1,1,2,2,4,4,5,5};
+        List<int[]> arr = Arrays.asList(player1, player2, player3);
+        for(int i=0; i<arr.size(); i++){
+            answerCheck(i,arr.get(i),answers);
+        }
+        int max = Math.max(count[0],Math.max(count[1],count[2]));
+        ArrayList<Integer>list = new ArrayList<>();
+        for(int i=0; i< count.length; i++){
+            if(max == count[i]){
                 list.add(i+1);
             }
         }
         return list;
     }
-    public int cal(int[] abc,int[] answers){
-          int count =0;
-           for(int i=0; i<answers.length;i++){
-               if(i<abc.length){
-                   if(abc[i] == answers[i]){
-                       count++;
-                   }
-               }else{
-                    if(abc[i%abc.length] == answers[i]){
-                        count++;
-                    }
-               }
-           }
-         return count; 
+    
+    static void answerCheck(int index, int[] player, int[] answers) {
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == player[i % player.length]) {
+                count[index]++;
+            }
         }
+    }
 }
