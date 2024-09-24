19e7a362a254ae0ece7610cc7847fb6529f74026
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-09-25 01:56:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 74.41 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 61.7 MB, 시간: 61.28 ms
+메모리: 62.2 MB, 시간: 74.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 01:00:03
+2024년 09월 25일 01:56:18
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -6,12 +6,16 @@ class Solution
         int answer = -1;
         Stack<Character> stack = new Stack();
         for(int i=0; i<s.length(); i++){
-               // 스택이 비어있지 않고 스택의 최상단 문자와 현재 문자가 같다면 짝지어 제거
-            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
-                stack.pop(); // 짝이 맞으면 제거
-            } else {
-                stack.push(s.charAt(i)); // 그렇지 않으면 스택에 현재 문자를 추가
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
             }
+            else if(stack.peek() == s.charAt(i)){
+                stack.pop();
+            }
+         else if(stack.peek() != s.charAt(i)){
+                stack.push(s.charAt(i));
+            }
+
         }
         
         if(stack.isEmpty()){
