99458f7c77009b876745a748fdb8a236c6211cf1
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-10-02 23:55:25 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 83.80 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,48 +1,41 @@
 import java.util.*;
-
 class Solution {
-    static Map<String, Integer> money = new HashMap<>();
-    static Map<String, String> graph = new HashMap<>();
-
+    static Map<String,Integer> money = new HashMap();
+    static Map<String,String> graph = new HashMap();
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = new int[enroll.length];
-        int profitPerSale = 100;
-
-        // enroll과 referral을 기반으로 graph와 money 초기화
-        for (int i = 0; i < enroll.length; i++) {
-            graph.put(enroll[i], referral[i]);
-            money.put(enroll[i], 0);
+        int cost = 100;
+        
+        for(int i=0; i<enroll.length; i++){
+            graph.put(enroll[i],referral[i]);
+            money.put(enroll[i],0);
         }
-
-        // 판매 기록에 따라 수익 계산
-        for (int i = 0; i < seller.length; i++) {
-            distributeProfit(seller[i], amount[i] * profitPerSale);
+        
+        for(int i=0; i<seller.length; i++){
+            check(seller[i],(amount[i]*cost));
         }
-
-        // 결과를 answer 배열에 저장
-        for (int i = 0; i < enroll.length; i++) {
+        for(int i=0; i<enroll.length; i++){
             answer[i] = money.get(enroll[i]);
         }
-
         return answer;
     }
-
-    static void distributeProfit(String seller, int profit) {
-        if (seller.equals("-")) {
+    static void check(String s,double num){
+        if(s.equals("-")){
             return;
+        }else{
+         double mine = Math.ceil(num*0.9);
+         double profit = num*0.1;
+         if(profit >= 1){
+            money.put(s,money.get(s)+(int)mine);
+                      check(graph.get(s),profit);  
+
+          }
+         else{
+           money.put(s,money.get(s)+(int)num);
+         }        
+          
         }
-
-        // 자신의 수익
-        int ownProfit = profit - profit / 10; // 90%
-        // 추천인에게 전달할 수익
-        int referralProfit = profit / 10; // 10%
-
-        // 자신의 수익 추가
-        money.put(seller, money.get(seller) + ownProfit);
-
-        // 추천인에게 수익 배분
-        if (referralProfit > 0) {
-            distributeProfit(graph.get(seller), referralProfit);
-        }
+        
+       
     }
 }
