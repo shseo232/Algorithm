e2e5167c7f37a0a4f321aeb78ea3c73b4dd2c7da
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-10-11 16:19:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.53 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,50 @@
 import java.util.*;
 class Solution {
     static boolean[][] visited;
-    static int[] dx = new int[]{-1,0,1,0};
-    static int[] dy = new int[]{0,1,0,-1};
-    static int[][] map;
-    static int n;
-    static int m;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+    static List<Integer> list = new ArrayList();
+    static int answer = 0;
     public int solution(int[][] maps) {
-        int answer = 0;
-        n = maps.length;
-        m = maps[0].length;
-        visited = new boolean[n][m];
-        bfs(0,0,maps);
-         if (maps[n-1][m-1] == 1) {
-            answer = -1;
-        } else {
-            answer = maps[n-1][m-1];
+        int width = maps[0].length;
+        int height = maps.length;
+        visited = new boolean[height][width];
+        bfs(maps,width,height);
+        if(answer == 0){
+            return -1;
         }
         return answer;
     }
-    static void bfs(int a, int b, int[][] maps){
+    public static void bfs(int[][] maps,int width, int height){
         Queue<int[]> q = new LinkedList();
-        q.add(new int[]{a,b});
-        visited[a][b] = true;
-    
+        int[] temp = new int[3];
+        temp[0] = 0;
+        temp[1] = 0;
+        temp[2] = 1;
+        q.add(temp);
+        visited[0][0] = true;
         while(!q.isEmpty()){
-            int[] cur = q.poll();
+            int[] temp1 = q.poll();
+            int x = temp1[0];
+            int y = temp1[1];
+            int d = temp1[2];
             for(int i=0; i<4; i++){
-                int x = cur[0] + dx[i];
-                int y = cur[1] + dy[i];
-                if(x >=0 && y >=0 && x<n && y<m){
-                    if(maps[x][y] ==1 && visited[x][y] == false){
-                        visited[x][y] = true;
-                        maps[x][y] = maps[cur[0]][cur[1]]+1;
-                        q.add(new int[]{x,y});
+                int currentX = x + dx[i];
+                int currentY = y + dy[i];
+                if(currentX >=0 && currentY >=0 && currentX < width && currentY < height){
+                    if(maps[currentY][currentX] == 1 && visited[currentY][currentX] == false){
+                        visited[currentY][currentX] = true;
+                        if(currentX == width-1 &&  currentY == height-1){
+                            answer = d+1;
+                        }
+                        int[] temp2 = new int[3];
+                        temp2[0] = currentX;
+                        temp2[1] = currentY;
+                        temp2[2] = d+1;
+                        q.add(temp2);
                     }
                 }
             }
         }
-        
     }
 }
